3760e601cfe50a79c5920896fce477ef80fbfc7f
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-10-04 12:20:44 -0500

    added resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,1 +1,40 @@
-import React from 
+import React from 'react';
+import Resume from '../../images/Resume';
+
+function Resume () {
+    return (
+    <div>
+        <button href={Resume}>Click for Resume</button>
+        <h2>Skills</h2>
+        <table>
+            <tbody>
+                <tr>
+                    <td>JavaScript</td>
+                    <td>Server Side APIs</td>
+                </tr>
+                <tr>
+                    <td>CSS/HTML</td>
+                    <td>Express.js</td>
+                </tr>
+                <tr>
+                    <td>jQuery</td>
+                    <td>OOP</td>
+                </tr>
+                <tr>
+                    <td>Node.js</td>
+                    <td>MVC</td>
+                </tr>
+                <tr>
+                    <td>TDD</td>
+                    <td>Jest</td>
+                </tr>
+                <tr>
+                    <td>SQL/mySQL/NoSQL</td>
+                    <td>ORM</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+    )
+}
