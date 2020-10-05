006074027ef97aaa59d7bb1b79a175ae9ff760e9
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-10-04 20:19:54 -0500

    Updates

---
+++ precache-manifest.de754db5c5ae00e247550ba82617e3bf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5be253ef8e7f9355d95662ee50ec8ca",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0750d469395ba90a5621",
+    "url": "/react-portfolio/static/css/main.e4f632c2.chunk.css"
+  },
+  {
+    "revision": "11c74771c5e82caec572",
+    "url": "/react-portfolio/static/js/2.f890d812.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.f890d812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0750d469395ba90a5621",
+    "url": "/react-portfolio/static/js/main.d32144c3.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "6bb72e4c0ebfd37f70424629cb5a9069",
+    "url": "/react-portfolio/static/media/item-1.6bb72e4c.png"
+  },
+  {
+    "revision": "24fc9b471392e50b368654d72f497c03",
+    "url": "/react-portfolio/static/media/item-2.24fc9b47.png"
+  },
+  {
+    "revision": "4424b30e5f3834aeb60b05948a8466e1",
+    "url": "/react-portfolio/static/media/item-3.4424b30e.png"
+  },
+  {
+    "revision": "55c68fc7a47b471ca8cd719bbcf7a24b",
+    "url": "/react-portfolio/static/media/item-4.55c68fc7.png"
+  },
+  {
+    "revision": "d5b97145d7faf3ce2891a30a2bce3a73",
+    "url": "/react-portfolio/static/media/item-5.d5b97145.png"
+  },
+  {
+    "revision": "d5b97145d7faf3ce2891a30a2bce3a73",
+    "url": "/react-portfolio/static/media/item-6.d5b97145.png"
+  },
+  {
+    "revision": "2c3b4f10a59b781ae3f176bdaf2e1390",
+    "url": "/react-portfolio/static/media/melissa-cadena.2c3b4f10.jpg"
+  },
+  {
+    "revision": "7c1d9cee65d87a8eee65182f69d4a8c7",
+    "url": "/react-portfolio/static/media/myResume.7c1d9cee.pdf"
+  },
+  {
+    "revision": "4dd0308a0c6a94c3487e30e1184d38e5",
+    "url": "/react-portfolio/static/media/world-wide-web.4dd0308a.png"
+  }
+]);
