b668dd7d2cc382e293ea1841aff9f2f256ba4186
Melissa Cadena <melissabcadena@gmail.com>
2020-11-16 17:01:55 -0600

    edited style

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,43 +1,15 @@
 import React from 'react';
-import MyResume from '../../images/myResume.pdf';
+import MyResume from '../../images/Resume.png';
+
 
 function Resume () {
     return (
-    <div className="flex-container skills">
-        <div className="flex-row resume-div">
-            <a className="resume" href={MyResume}>Click for Resume</a>
-        </div>
-        <div className="flex-row">
-            <h2>Skills</h2>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>JavaScript</td>
-                        <td>Server Side APIs</td>
-                    </tr>
-                    <tr>
-                        <td>CSS/HTML</td>
-                        <td>Express.js</td>
-                    </tr>
-                    <tr>
-                        <td>jQuery</td>
-                        <td>OOP</td>
-                    </tr>
-                    <tr>
-                        <td>Node.js</td>
-                        <td>MVC</td>
-                    </tr>
-                    <tr>
-                        <td>TDD</td>
-                        <td>Jest</td>
-                    </tr>
-                    <tr>
-                        <td>SQL/mySQL/NoSQL</td>
-                        <td>ORM</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+    <div className="flex-container resume">
+        <img
+        src={MyResume}
+        alt="Resume">
+        
+        </img>
     </div>
     
     )
